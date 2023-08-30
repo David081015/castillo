4f85f8bd530753326a5a4b2884007622849b218b
David081015 <byjoker19.play@gmail.com>
2023-08-30 08:02:25 -0500

    NPM

--- Parcial 1/pknpm19/index.js
+++ Parcial 1/pknpm19/index.js
@@ -1,7 +1,7 @@
 let equipos = ["Cruz Azul", "Toluca", "Santos"]
 
 function obtenerEquipo(indice) {
-    return equipos(indice);
+    return equipos[indice];
 }
 
 module.exports.obtenerEquipo = obtenerEquipo;
