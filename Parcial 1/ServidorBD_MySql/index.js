e97185009de2012451d045fd3fa103385e5a0ed2
David081015 <byjoker19.play@gmail.com>
2023-09-13 22:54:21 -0500

    Listo

--- Parcial 1/ServidorBD_MySql/index.js
+++ Parcial 1/ServidorBD_MySql/index.js
@@ -25,6 +25,18 @@ const dataDeBase = {
     database: 'alumnos',
 };
 
+//ALL USERS
+app.get('/alumnos', async (req, resp) => {
+    try {
+        const conexion = await mysql.createConnection(dataDeBase);
+        const [rows, fields] = await conexion.query('SELECT * FROM tec');
+        resp.json(rows);
+    } catch (err) {
+        resp.status(500).json({ mensaje: 'Error de conexión', tipo: err.message, sql: err.sqlMessage });
+    }
+});
+
+//SELECT
 app.get('/alumnos/:id', async (req, resp) => {
     try {
         console.log(req.params.id);
@@ -41,6 +53,60 @@ app.get('/alumnos/:id', async (req, resp) => {
     }
 });
 
-app.listen(8080, () => {
+//INSERT INTO
+// Ruta para agregar un usuario con parámetros de consulta
+app.post('/alumnos', async (req, resp) => {
+    try {
+        const id = req.query.idtec;
+        const nom = req.query.nombre;
+        const ape = req.query.apellido;
+        const conexion = await mysql.createConnection(dataDeBase);
+        const [result] = await conexion.query('INSERT INTO tec (idtec, nombre, apellido) VALUES (?, ?, ?)', [id, nom, ape]);
+        
+        resp.status(201).json({ mensaje: 'Usuario agregado correctamente'});
+    } catch (err) {
+        resp.status(500).json({ mensaje: 'Error de conexión', tipo: err.message, sql: err.sqlMessage });
+    }
+});
+
+//UPDATE
+app.put('/alumnos', async (req, resp) => {
+    try {
+        const id = req.query.idtec;
+        const nom = req.query.nombre;
+        const ape = req.query.apellido;
+        const conexion = await mysql.createConnection(dataDeBase);
+        const [result] = await conexion.query('UPDATE tec SET nombre=?, apellido=? WHERE idtec=?', [nom, ape, id]);
+
+        if (result.affectedRows === 0) {
+            resp.status(404).json({ mensaje: 'Usuario no encontrado' });
+        } else {
+            resp.json({ mensaje: 'Usuario modificado correctamente' });
+        }
+    } catch (err) {
+        resp.status(500).json({ mensaje: 'Error de conexión', tipo: err.message, sql: err.sqlMessage });
+    }
+});
+
+
+//DELETE
+app.delete('/alumnos', async (req, resp) => {
+    try {
+        const id = req.query.idtec;
+        console.log(id);
+        const conexion = await mysql.createConnection(dataDeBase);
+        const query = "DELETE FROM tec WHERE idtec = "+id;
+        const [rows, fields] = await conexion.query(query);
+        if (rows.affectedRows === 0) {
+            resp.json({ mensaje: 'Registro No Eliminado' });
+        } else {
+            resp.json({ mensaje: 'Registro Eliminado' });
+        }
+    } catch (err) {
+        resp.status(500).json({ mensaje: 'Error de conexión', tipo: err.message, sql: err.sqlMessage });
+    }
+});
+
+app.listen(8084, () => {
     console.log('Servidor express escuchando');
 });
