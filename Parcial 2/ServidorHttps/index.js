bfb3ab9c51e4a7c8a309cd3f9e1c11eb125f846f
David081015 <byjoker19.play@gmail.com>
2023-10-17 08:44:34 -0500

    SSL

---
+++ Parcial 2/ServidorHttps/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const opciones = {
+    key: fs.readFileSync(path.join(__dirname,"SSL/key.pem")),
+    cert: fs.readFileSync(path.join(__dirname,"SSL/cert.pem"))
+}
+const app = express();
+
+app.get('/ssl', function(req,res){
+    res.json({respuesta: "Peticion get segura"})
+});
+
+/*
+app.listen(8084, function(){
+    console.log("Server Express seguro puerto 8084")
+})
+*/
+
+https.createServer(opciones,app).listen(8084,function(){
+    console.log("Servidor Express seguro en puerto 8084")
+})
